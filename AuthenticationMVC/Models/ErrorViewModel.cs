40241b1d093e03010cdf2cb37409af95778a895b
tanqueta88 <oliverprim783@gmail.com>
2024-04-21 05:42:29 -0300

    Project folders are refactored

--- AuthenticationMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthenticationMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
