7589ea48352acae6d508b8d43b97380ccdadb800
tanqueta88 <oliverprim783@gmail.com>
2024-04-23 01:19:09 -0300

    Modifications in script file is added

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let character = 'Hello';
-let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0]);
-rows[2] = 10;
-rows.length
+const character = "#";
+const count = 8;
+const rows = [];
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(character.repeat(i))
+  }
+  let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
